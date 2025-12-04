e485eecd30569065047e1db51f5e537ce8811280
Taras Kovalyk <tarik2454@gmail.com>
2025-12-04 13:43:47 +0200

    feat: create project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}></div>
+    </div>
+  );
+}
