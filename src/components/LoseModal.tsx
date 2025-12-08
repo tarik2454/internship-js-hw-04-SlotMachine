6f33dfca72264812ad732970e6b49f0b38db8d08
Taras Kovalyk <tarik2454@gmail.com>
2025-12-08 17:59:20 +0200

    feat: enhance LoseModal with rain effect animation, update layout and styles, and improve responsiveness for mobile devices

--- src/components/LoseModal.tsx
+++ src/components/LoseModal.tsx
@@ -3,9 +3,11 @@
 import React from "react";
 import { useSlotLogic } from "../hooks/useSlotLogic";
 import styles from "./LoseModal.module.scss";
+import Image from "next/image";
+import youLose from "../image/modal/you-lose.svg";
 
 export const LoseModal = () => {
-  const { gameResult, handleResetGame } = useSlotLogic();
+  const { gameResult, handleResetGame, currentBet } = useSlotLogic();
 
   if (gameResult !== "lose") return null;
 
@@ -16,23 +18,23 @@ export const LoseModal = () => {
   };
 
   return (
-    <div
-      className={`${styles.modalOverlay} ${styles.loseBackground}`}
-      onClick={handleOverlayClick}
-    >
-      <div className={styles.modal}>
-        <div className={styles.modalContent}>
-          <div className={styles.loseIcon}>😔</div>
-          <h2 className={styles.modalTitle}>TRY AGAIN!</h2>
-          <p className={styles.loseMessage}>Better luck next time</p>
-          <button
-            className={styles.closeButton}
-            onClick={handleResetGame}
-            type="button"
-          >
-            SPIN AGAIN
-          </button>
-        </div>
+    <div className={styles.modalOverlay} onClick={handleOverlayClick}>
+      <div className={styles.rainEffect}>
+        {Array.from({ length: 60 }, (_, i) => (
+          <div key={i} className={styles.rainDrop}></div>
+        ))}
+      </div>
+      <div>
+        <Image src={youLose} className={styles.loseImage} alt="Lose icon" />
+
+        <p className={styles.loseAnmount}>-{currentBet}</p>
+        <button
+          className={styles.closeButton}
+          onClick={handleResetGame}
+          type="button"
+        >
+          SPIN AGAIN
+        </button>
       </div>
     </div>
   );
