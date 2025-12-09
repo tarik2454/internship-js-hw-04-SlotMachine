260a4b677fcc5bb0e0582b85d68f0591478b9dd7
Taras Kovalyk <tarik2454@gmail.com>
2025-12-09 21:37:22 +0200

    fix: update import paths in Balance component to use absolute paths for better module resolution

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 import { useSlotLogic } from "../hooks/useSlotLogic";
 import { useSlotStore } from "../store/useSlotStore";
 import styles from "./Balance.module.scss";
-import balanceBase from "../image/balance/balance-base.svg";
-import balanceIcon from "../image/balance/balance.svg";
-import jackpotIcon from "../image/balance/jackpot.svg";
+import balanceBase from "@/image/balance/balance-base.svg";
+import balanceIcon from "@/image/balance/balance.svg";
+import jackpotIcon from "@/image/balance/jackpot.svg";
 
 export const Balance = () => {
   const { balance, jackpot, lastWin } = useSlotLogic();
